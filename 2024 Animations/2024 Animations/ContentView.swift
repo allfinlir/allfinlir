59796dcbd75b9dfeacf35df25933f48adf076a67
allfinlir <89570027+allfinlir@users.noreply.github.com>
2024-01-01 14:35:46 +0100

    Add files via upload

---
+++ 2024 Animations/2024 Animations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2024 Animations
+//
+//  Created by Rikard Garcia Wernersson on 2024-01-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2024 Animations/2024 Animations/_024_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _024_AnimationsApp.swift
+//  2024 Animations
+//
+//  Created by Rikard Garcia Wernersson on 2024-01-01.
+//
+
+import SwiftUI
+
+@main
+struct _024_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
